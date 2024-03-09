8b2236e34d87b7d5e780b791263c58275a2889d2
bahati-hakizimana <hakizimanabahati2019@gmail.com>
2024-03-09 12:22:04 -0800

    Empower africa02

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,29 +7,31 @@ const Banner = () => {
       <section data-aos="fade-up" className="container py-8 md:py-12">
         <div className="grid grid-cols-1 items-center gap-4 sm:text-ellipsis md:grid-cols-3 md:gap-8">
           <div className=" order-2 flex flex-col items-center gap-4 text-center text-white md:col-span-2  md:items-start md:text-left ">
-            <h1 className="text-3xl font-bold ">Market your Brand</h1>
+            <h1 className="text-3xl font-bold ">Boost Your Brand's Visibility</h1>
             <p className="">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
-              ducimus eius quis ad enim aliquid iusto eum nihil exercitationem
-              necessitatibus?
+              Elevate your brand with strategic marketing. Increase your reach and <br />
+              engagement, attract new customers, and strengthen your brand identity. <br />
+              Empower Africa  helps to archive it.
             </p>
+
             <a href="https://www.youtube.com/watch?v=PzhWDSW6yzg" target="blank">
-            <button className="rounded-md bg-white px-4 py-2 text-sm  text-black transition-colors duration-300 hover:bg-white/90">
-              Get Started
-            </button>
+              <button className="rounded-md bg-white px-4 py-2 text-sm  text-black transition-colors duration-300 hover:bg-white/90">
+                Get Started
+              </button>
             </a>
-            
+
           </div>
           <div className="order-1">
             <iframe
               className="aspect-video w-full"
-              src="https://www.youtube.com/embed/gRWMen27Uio?si=VtHMh9xCxQ6ccFh8"
+              src="https://www.youtube.com/embed/PzhWDSW6yzg"
               title="YouTube video player"
               frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowfullscreen
             ></iframe>
           </div>
+
         </div>
       </section>
     </main>
