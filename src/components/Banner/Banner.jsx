e700766043d77faa2608612b1e5f085dd8505dc9
bahati-hakizimana <hakizimanabahati2019@gmail.com>
2024-03-09 12:11:53 -0800

    Empower africa0

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -13,9 +13,12 @@ const Banner = () => {
               ducimus eius quis ad enim aliquid iusto eum nihil exercitationem
               necessitatibus?
             </p>
+            <a href="https://www.youtube.com/watch?v=PzhWDSW6yzg" target="blank">
             <button className="rounded-md bg-white px-4 py-2 text-sm  text-black transition-colors duration-300 hover:bg-white/90">
               Get Started
             </button>
+            </a>
+            
           </div>
           <div className="order-1">
             <iframe
